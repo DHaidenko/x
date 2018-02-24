af9082a1dc2d854bf4e6bb55cec064ef6064a993
DHaidenko <dgaidenko@gmail.com>
2018-02-24 13:45:38 +0200

    Junit asserts

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,11 +1,33 @@
+import org.hamcrest.core.Is;
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.matchers.JUnitMatchers.both;
+import static org.junit.matchers.JUnitMatchers.containsString;
 
 /**
  * Created by Student on 24.02.2018.
  */
+@RunWith(Parameterized.class)
 public class MyTest {
+int a;
+    int b;
 
+    public MyTest(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
 
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList( new Object[][]{{2,4},{2,4},{3,6},{5,10}});
+
+    }
 
     @BeforeClass
     public static void setUpClass() throws Exception {
@@ -26,19 +48,13 @@ public class MyTest {
         System.out.println("af");
     }
 
-    @Test
-    public void test1() {
-        System.out.println("test");
-    }
 
-    @Test
-    public void test2() {
-        System.out.println("df");
-    }
+
+
 
     @Test
     public void test3() {
-
-        System.out.println("default");
+        int i = a+a;
+        Assert.assertEquals("as",b,i);
     }
 }
